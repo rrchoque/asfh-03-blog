d733a42c71b1c3dec002cb4a480da8436389a020
Reynaldo Rodrigo Choque Vicente <reynaldorodrigochoque@gmail.com>
2025-01-08 16:23:19 -0400

    Se creó la relación entre los post y los autores

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,4 +1,4 @@
-import { defineCollection, z } from "astro:content";
+import { defineCollection, reference, z } from "astro:content";
 
 const blogCollection = defineCollection({
   type: "content",
@@ -10,13 +10,24 @@ const blogCollection = defineCollection({
       image: image(),
 
       // Relación
-      author: z.string(),
+      //author: z.string(),
+      author: reference("author"),
 
       // Relación
       tags: z.array(z.string()),
     }),
 });
 
+const authorCollection = defineCollection({
+  type: "data",
+  schema: ({ image }) =>
+    z.object({
+      name: z.string(),
+      avatar: image(),
+    }),
+});
+
 export const collections = {
   blog: blogCollection,
+  author: authorCollection,
 };
