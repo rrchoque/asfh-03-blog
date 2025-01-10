4d8adcc9cdbf3bbe839c155ded1c43ea2dbb18c0
Reynaldo Rodrigo Choque Vicente <reynaldorodrigochoque@gmail.com>
2025-01-10 07:44:47 -0400

    Se agregó content e image al rss

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,20 +1,51 @@
 import { sync, type APIRoute } from "astro";
 import rss from "@astrojs/rss";
 import { getCollection } from "astro:content";
+import MarkdownIt from "markdown-it";
+import xss from "xss";
+
+const parser = new MarkdownIt();
 
 export const GET: APIRoute = async ({ params, request, site }) => {
   const blogPosts = await getCollection("blog");
-  const items = blogPosts.map(({ data, slug }) => ({
-    title: data.title,
-    pubDate: data.date,
-    description: data.description,
-    link: `/posts/${slug}`,
-  }));
+
+  // const items = blogPosts.map(({ data, slug, body }) => ({
+  //   title: data.title,
+  //   pubDate: data.date,
+  //   description: data.description,
+  //   link: `/posts/${slug}`,
+  //   content: sanitizeHtml(parser.render(body), {
+  //     allowedTags: sanitizeHtml.defaults.allowedTags.concat(["img"]),
+  //   }),
+  // }));
+
+  const items = blogPosts.map(({ data, slug, body }) => {
+    const content = xss(parser.render(body)); // Usando xss para limpiar el HTML
+
+    return {
+      title: data.title,
+      pubDate: new Date(data.date).toUTCString(),
+      description: data.description,
+      link: `${site}/posts/${slug}`,
+      content: content,
+      customData: `<media:content 
+      url="${site + data.image.src}"
+      width="${data.image.width}"
+      height="${data.image.height}"
+      medium="image"
+      type="image/${data.image.format}"
+       />`,
+    };
+  });
+
   return rss({
     // `<title>` field in output xml
     title: "Reynaldo Blog",
     // `<description>` field in output xml
     description: "A humble Astronaut’s guide to the stars",
+    xmlns: {
+      media: "http://search.yahoo.com/mrss/",
+    },
     // Pull in your project "site" from the endpoint context
     // https://docs.astro.build/en/reference/api-reference/#site
     site: site ?? "",
@@ -22,6 +53,6 @@ export const GET: APIRoute = async ({ params, request, site }) => {
     // See "Generating items" section for examples using content collections and glob imports
     items: items,
     // (optional) inject custom xml
-    customData: `<language>es-Es</language>`,
+    customData: `<language>es-ES</language>`,
   });
 };
